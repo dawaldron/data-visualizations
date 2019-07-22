964cb0ff8c370c7a04d6e73f3a5ee6f77a60ba6e
dawaldron <dwaldron89@gmail.com>
2019-07-21 20:35:37 -0400

    update to delete missings from file

--- cps_correction/cps_correction.R
+++ cps_correction/cps_correction.R
@@ -4,9 +4,10 @@
 # on 2019-07-17
 
 # Hur and Achen correction:
-# For repondents reporting not voting (VOTED == 1), compute correction = (1-VEP)/(1-CPS state level turnout rate)
-# For repondents reporting voting (VOTED == 2), compute correction = VEP/CPS state level turnout rate excluding missing data, using WTFINL, the CPS individual rate
-# multiply correction, multiplying by WTFINL to produce new weight
+# 1. Delete records missing (VOTED not in 1,2)
+# 2. For repondents not voting (VOTED == 1), compute correction = (1-VEP)/(1-CPS state level turnout rate excluding missing data)
+# 3. For repondents voting (VOTED == 2), compute correction = VEP/CPS state level turnout rate excluding missing data
+# 4. Multiply WTFINL by correction to produce new weight (WTFINL2)
 
 library(data.table)
 library(magrittr)
@@ -20,13 +21,12 @@ dt_fips <- fread('state_icpsr_fips.txt')
 # IPUMS-CPS extract link to recreate:
 # https://cps.ipums.org/cps-action/extract_requests/5215536/revise
 dt_cps <- fread('cps_00053.csv') %>%
-  .[AGE >= 18 & VOTED != 99]
+  .[AGE >= 18 & VOTED %in% 1:2] # delete missing VOTED
 
-# calculate turnout with VOTED (PES1 in Census file)
-# and match to electproject VEP turnout
+# calculate turnout and match to electproject VEP turnout
 dt_turnout <- dt_cps %>%
   .[,
-    .(cps_turnout = sum(WTFINL * (VOTED == 2)) / sum(WTFINL * (VOTED %in% c(1,2)))),
+    .(cps_turnout = sum(WTFINL * (VOTED == 2)) / sum(WTFINL)),
     .(YEAR, STATEFIP)] %>%
   dt_fips[., on = 'STATEFIP', nomatch = 0] %>%
   dt_vep[., on = c('YEAR','STATEICP','STATENM'), nomatch = 0]
@@ -53,8 +53,14 @@ dt_cps[EDUC %in% c(80,81,90,91,92,100), educattain := 'Some college, no BA']
 dt_cps[EDUC %in% c(110,111,121,122,123,124,125), educattain := 'Bachelor\'s degree']
 
 # recode age
-dt_cps[, agegrp := paste0(floor((AGE - 18) / 4) * 4 + 18, '-',
-                          floor((AGE - 18) / 4) * 4 + 21)]
+dt_cps[, agegrp := '18-29']
+dt_cps[AGE >= 30, agegrp := '30-44']
+dt_cps[AGE >= 45, agegrp := '45-59']
+dt_cps[AGE >= 60, agegrp := '60+']
+
+# 4-year age groups
+dt_cps[, agegrp4y := paste0(floor((AGE - 18) / 4) * 4 + 18, '-',
+                            floor((AGE - 18) / 4) * 4 + 21)]
 
 # recode race/ethnicity
 dt_cps[, race := 'Other']
@@ -65,7 +71,7 @@ dt_cps[!HISPAN %in% c(0,901,902), race := 'Hispanic']
 # turnout by year, state (check that numbers match electproject.org)
 dt_state <- dt_cps %>%
   .[,
-    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2 * (VOTED %in% c(1,2))), 1)),
+    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2), 1)),
     .(YEAR, STATEFIP, STATENM)] %>%
   dcast(YEAR ~ STATENM, value.var = 'turnout')
 
@@ -74,7 +80,7 @@ fwrite(dt_state, 'state.csv')
 # turnout by year, educ
 dt_educ <- dt_cps %>%
   .[,
-    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2 * (VOTED %in% c(1,2))), 1)),
+    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2), 1)),
     .(YEAR, educattain)] %>%
   dcast(YEAR ~ educattain, value.var = 'turnout')
 
@@ -83,19 +89,28 @@ fwrite(dt_educ, 'educ.csv')
 # turnout by year, race
 dt_race <- dt_cps %>%
   .[,
-    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2 * (VOTED %in% c(1,2))), 1)),
+    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2), 1)),
     .(YEAR, race)] %>%
   dcast(YEAR ~ race, value.var = 'turnout')
 
 fwrite(dt_race, 'race.csv')
 
-# turnout by year, age
+# turnout by year, age (4 groups)
 dt_age <- dt_cps %>%
   .[,
-    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2 * (VOTED %in% c(1,2))), 1)),
+    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2), 1)),
     .(YEAR, agegrp)] %>%
   dcast(YEAR ~ agegrp, value.var = 'turnout')
 
 fwrite(dt_age, 'age.csv')
+
+# turnout by year, age (4-year groups)
+dt_age4y <- dt_cps %>%
+  .[,
+    .(turnout = round(100 * sum(WTFINL2 * (VOTED == 2)) / sum(WTFINL2), 1)),
+    .(YEAR, agegrp4y)] %>%
+  dcast(YEAR ~ agegrp4y, value.var = 'turnout')
+
+fwrite(dt_age4y, 'age4y.csv')
 
 
